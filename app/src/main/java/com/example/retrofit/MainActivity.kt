3f73867c775bb5fbb36ff4395ccd09f7b8805292
Andrew Weiss <andreivayss@gmail.com>
2021-09-12 10:37:16 +0700

    Added dynamic requests by id

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -2,40 +2,34 @@ package com.example.retrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.retrofit.repository.Repository
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    var content: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        content = findViewById(R.id.textView)
-
         val repository = Repository()
         val viewModelFactory = MainViewModelFactory(repository)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
-        viewModel.getPost()
-        viewModel.myResponse.observe(this, Observer { response ->
-            if (response.isSuccessful) {
-                Log.d("Response", "1")
-                Log.d("Response", response.body()?.myUserId.toString())
-                Log.d("Response", response.body()?.id.toString())
-                content?.text = response.body()?.title!!
-                Log.d("Response", response.body()?.body.toString())
-            } else {
-                Log.d("Response", "2")
-                Log.d("Response", response.errorBody().toString())
-                content?.text = response.code().toString()
-            }
-        })
-        Log.d("Response", "Does it work?")
+
+        button.setOnClickListener{
+            val myNumber = number_editText.text.toString()
+            viewModel.getPost2(Integer.parseInt(myNumber))
+            viewModel.myResponse2.observe(this, Observer { response ->
+                if (response.isSuccessful) {
+                    textView.text = response.body().toString()
+                } else {
+                    textView.text = response.code().toString()
+                }
+            })
+        }
     }
 }
